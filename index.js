18c8df56490afc4f333281bb5f1307d5ef007162
Anike <adeoyesumisola@gmail.com>
2025-06-12 21:53:46 +0100

    java commit

--- index.js
+++ index.js
@@ -260,3 +260,10 @@ switch (role) {
 for (let i = 1; i <= 5; i++) {
     if (i % 2 !== 0)  console.log(i);
 }
+
+//while loop
+let i = 0;
+while (i <= 5) {
+    if (i % 2 !== 0) console.log(i);
+    i++;
+}
